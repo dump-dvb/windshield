38e04011c084933f9c9f3f4ba542c88634b79001
Astro <astro@spaceboyz.net>
2022-05-02 01:09:30 +0200

    compose id of line and run_number

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ window.onload = function() {
 
     const bounds = pointToBounds(data)
 
-    const id = data.run_number
+    const id = data.line + "-" + data.run_number
     if (!vehicles[id] && bounds) {
       var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg")
       svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg")
